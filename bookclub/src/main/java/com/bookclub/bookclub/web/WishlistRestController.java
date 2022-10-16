c50e590e69e24065a319633a9a2d4f7f5c9a026a
JANZAL KARLO SONZA <jkgarciasonza@my365.bellevue.edu>
2022-10-16 14:32:31 -0700

    Added new and updated files for week 7.

---
+++ bookclub/src/main/java/com/bookclub/bookclub/web/WishlistRestController.java
@@ -0,0 +1,37 @@
+package com.bookclub.bookclub.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookclub.bookclub.service.dao.WishlistDao;
+import com.bookclub.bookclub.service.impl.MongoWishlistDao;
+import com.bookclub.bookclub.model.WishlistItem;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RestController
+@RequestMapping(path = "/api/wishlist", produces = "application/json")
+@CrossOrigin(origins = "*")
+public class WishlistRestController {
+    WishlistDao wishlistDao = new MongoWishlistDao();
+
+    @Autowired
+    private void setWishlistDao(WishlistDao wishlistDao) {
+        this.wishlistDao = wishlistDao;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String showWishlist() {
+        return "wishlist/list";
+    }
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public WishlistItem findById(@PathVariable String id) {
+        return wishlistDao.find(id);
+    }
+}
